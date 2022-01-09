48daad7a2fe9f33797ed23178894f6838c8b1a78
monish karunakaran <ckmonish2000@gmail.com>
2022-01-09 20:06:02 +0530

    working on with draw

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { ethers } from "ethers"
 import Posts from "./artifacts/contracts/Posts.sol/Posts.json"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const Post_contract = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
 
@@ -10,19 +10,24 @@ function App() {
   const [title, setTitle] = useState("")
   const [desc, setDesc] = useState("")
   const [image, setImage] = useState("")
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    get_All_Posts()
+  }, [])
 
   async function requestAccount() {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
     console.log(accounts)
   }
 
-  async function get_All_User_Posts() {
+  async function get_All_Posts() {
     if (window.ethereum !== undefined) {
       const provider = new ethers.providers.Web3Provider(window.ethereum)
       const contract = new ethers.Contract(Post_contract, Posts.abi, provider)
       try {
         const data = await contract.get_Posts()
-        console.log(data)
+        setPosts(data)
       } catch (err) {
         console.log(err)
       }
@@ -40,14 +45,12 @@ function App() {
       const contract = new ethers.Contract(Post_contract, Posts.abi, signer)
       const transaction = await contract.createPost(title, desc, image)
       await transaction.wait()
-      get_All_User_Posts()
+      get_All_Posts()
     }
   }
 
 
   async function sendAmount() {
-
-
     if (window.ethereum !== undefined) {
       await requestAccount()
       const provider = new ethers.providers.Web3Provider(window.ethereum)
@@ -56,20 +59,14 @@ function App() {
       const options = { value: ethers.utils.parseEther("1.0") }
       const transaction = await contract.donate(0, options)
       await transaction.wait()
-      get_All_User_Posts()
+      get_All_Posts()
     }
   }
-  // async function Provider_check() {
-  //   const provider = new ethers.providers.Web3Provider(window.ethereum)
-  //   console.log(window.ethereum._state)
-  // }
-  console.log(ethers.utils.formatEther("0x0de0b6b3a7640000"))
 
+  console.log(posts)
   return (
     <div className="App">
       <button onClick={requestAccount} className="">Connect Wallet</button>
-      <button onClick={get_All_User_Posts}>get Posts</button>
-
 
       <div className="box">
         <input
@@ -90,7 +87,23 @@ function App() {
 
         <button onClick={handle_submit}>submit</button>
       </div>
-      <button onClick={sendAmount}>Donate</button>
+
+
+      <div className="posts_box">
+        {
+          posts.map(v => {
+            return (
+              <div className="card">
+                <h1>{v?.title}</h1>
+                <div>{v?.content}</div>
+                <button onClick={sendAmount}>Donate</button>
+                {v?.author.toLowerCase() === window.ethereum._state.accounts[0] && <button onClick={sendAmount}>Withdraw</button>}
+              </div>
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
