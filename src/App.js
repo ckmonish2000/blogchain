36902af57bfdf8940b8e3ffa033fac878a8a7e62
monish karunakaran <ckmonish2000@gmail.com>
2022-01-09 17:23:06 +0530

    create post

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { ethers } from "ethers"
 import Posts from "./artifacts/contracts/Posts.sol/Posts.json"
+import { useState } from "react"
 
 const Post_contract = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
 const Blog_contarct = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [desc, setDesc] = useState("")
+  const [image, setImage] = useState("")
 
   async function requestAccount() {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -26,6 +30,21 @@ function App() {
       }
     }
   }
+
+
+  async function handle_submit() {
+    if (title === "" && desc === "" && image === "") return
+
+    if (window.ethereum !== undefined) {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(Post_contract, Posts.abi, signer)
+      const transaction = await contract.createPost(title, desc, image)
+      await transaction.wait()
+      get_All_User_Posts()
+    }
+  }
   // async function Provider_check() {
   //   const provider = new ethers.providers.Web3Provider(window.ethereum)
   //   console.log(window.ethereum._state)
@@ -36,6 +55,27 @@ function App() {
     <div className="App">
       <button onClick={requestAccount} className="">Connect Wallet</button>
       <button onClick={get_All_User_Posts}>get Posts</button>
+
+
+      <div className="box">
+        <input
+          placeholder="Title"
+          onChange={e => setTitle(e.target.value)} />
+
+        <textarea
+          rows="4"
+          cols="30"
+          onChange={e => setDesc(e.target.value)}
+        >
+        </textarea>
+
+        <input
+          placeholder="Image"
+          onChange={e => setImage(e.target.value)}
+        />
+
+        <button onClick={handle_submit}>submit</button>
+      </div>
     </div>
   );
 }
