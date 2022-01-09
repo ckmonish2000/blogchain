9a6f143509e0c2e5a95e0cd6b630f80a7d9b4777
monish karunakaran <ckmonish2000@gmail.com>
2022-01-09 20:15:33 +0530

    working stuff

--- src/App.js
+++ src/App.js
@@ -50,20 +50,31 @@ function App() {
   }
 
 
-  async function sendAmount() {
+  async function sendAmount(id) {
     if (window.ethereum !== undefined) {
       await requestAccount()
       const provider = new ethers.providers.Web3Provider(window.ethereum)
       const signer = provider.getSigner();
       const contract = new ethers.Contract(Post_contract, Posts.abi, signer)
       const options = { value: ethers.utils.parseEther("1.0") }
-      const transaction = await contract.donate(0, options)
+      const transaction = await contract.donate(id, options)
       await transaction.wait()
       get_All_Posts()
     }
   }
 
-  console.log(posts)
+  async function Withdraw(id) {
+    if (window.ethereum !== undefined) {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(Post_contract, Posts.abi, signer)
+      const transaction = await contract.withdraw_funds(id)
+      await transaction.wait()
+      get_All_Posts()
+    }
+  }
+
   return (
     <div className="App">
       <button onClick={requestAccount} className="">Connect Wallet</button>
@@ -91,13 +102,13 @@ function App() {
 
       <div className="posts_box">
         {
-          posts.map(v => {
+          posts.map((v, idx) => {
             return (
-              <div className="card">
-                <h1>{v?.title}</h1>
+              <div className="card" key={idx}>
+                <h1>{v?.title} ({ethers.utils.formatEther(v?.current?._hex)}ETH)</h1>
                 <div>{v?.content}</div>
-                <button onClick={sendAmount}>Donate</button>
-                {v?.author.toLowerCase() === window.ethereum._state.accounts[0] && <button onClick={sendAmount}>Withdraw</button>}
+                <button onClick={() => { sendAmount(idx) }}>Donate</button>
+                {v?.author.toLowerCase() === window.ethereum._state.accounts[0] && <button onClick={() => { Withdraw(idx) }}>Withdraw</button>}
               </div>
             )
           })
